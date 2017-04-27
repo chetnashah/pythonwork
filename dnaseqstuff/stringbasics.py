d95c54396e59bfcc1a0096f6149a442471e45b92
chetnashah <chetshah80@gmail.com>
2017-04-27 21:48:03 +0530

    missing commit

--- dnaseqstuff/stringbasics.py
+++ dnaseqstuff/stringbasics.py
@@ -22,3 +22,16 @@ for _ in range(10):
     seqrandom += random.choice('ACGT')
 print(seqrandom)
 
+li = ['A','C','A','T','G','A','T','G','G']
+# seperator.join(list)
+print(','.join(li))
+print(' '.join(li))
+print(''.join(li))
+
+
+# use split on string
+longstr = 'A,G,C,T,A,G,G,G,C,A,T,A,G'
+print(longstr.split(','))
+
+
+
